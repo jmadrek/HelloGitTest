24efd2452f222783a0a21e2d574a081748381d21
jmadrek <CTodd@UWAlumni.com>
2018-02-18 00:17:52 -0600

    Commit Branch 1

--- HelloGit/src/Main.java
+++ HelloGit/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
 		//1st save
 		System.out.println("Hello GIT");
 	}
+	
+	//Branch1 - Add Print Out To String
+	//Maybe we could print it in a method
+	public static void printString(String mString){
+		System.out.println(mString);
+	}
 
 }
